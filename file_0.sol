f0d00d8eb95903867123943769ac5dfb7221bf9f
Jan Pelicon <jnpelicon@gmail.com>
2025-02-12 17:07:02 +0100

    more files

---
+++ file_0.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+import "../utils/Context.sol";
+
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor() {
+        _transferOwnership(_msgSender());
+    }
+}
---
+++ file_1.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.0;
+
+import "../utils/Context.sol";
+
+abstract contract Ownable is Context {
+    address private _owner;
+}
